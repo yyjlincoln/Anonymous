b84eb5636938d0456da70cf05dccef1dec6d4f71
yyjlincoln <yyjlincoln@gmail.com>
2017-07-24 18:34:43 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,12 +6,14 @@
 ##People[[WechatID,Gender,RoomID or Status,[...]]
 #PeopleAll{'UserName':{'Gender':'Male/Female','Status':'RoomID'/False,'ToUser':''}}
 import itchat
+import time
 import random
 from itchat.content import *
 Room_Profile={}
 Room_id=[]
 People=[]
 PeopleAll={}
+GamePeople=[]
 print('Trying to log in Wechat Server Account.')
 itchat.auto_login(hotReload=True)
 print('Logged in.')
@@ -21,14 +23,17 @@ def message_recieved(msg):
     global Room_id
     global People
     global PeopleAll
+    global GamePeople
     #print(msg['User']['Sex'])
     print(msg['Content'])
     if msg['Content']=='开始':
         if msg['FromUserName'] in PeopleAll:
-            if PeopleAll[msg['FromUserName']]['Status']!=False:
+            if PeopleAll[msg['FromUserName']] in GamePeople:
                 print('In Game')
                 itchat.send_msg('#[系统提示]您还在聊天中,请先结束聊天.本消息仅您可见.已发送\"开始\"至对方。',toUserName=msg['FromUserName'])
                 itchat.send_msg(msg['content'],toUserName=PeopleAll[msg['FromUserName']]['ToUser'])
+            elif PeopleAll[msg['FromUserName']]['Status']==True:
+                itchat.send_msg('#[系统提示]您已经在等待列表中了.本消息仅您可见.',toUserName=msg['FromUserName'])
             else:
                 itchat.send_msg('正在加入,请稍等.',toUserName=msg['FromUserName'])
                 #更新数据
@@ -38,24 +43,62 @@ def message_recieved(msg):
                 if len(People)==1:
                     print('人不够')
                 itchat.send_msg('正在等待匹配.您也可以随时输入[离开]退出等待。',toUserName=msg['FromUserName'])
-                pair(msg)
+                #pair(msg)
         else:
              print(msg['User']['Sex'])
-             PeopleAll[msg['FromUserName']]={'Gender':msg['User']['Sex'],'Status':False,'ToUser':''}
+             PeopleAll[msg['FromUserName']]={'Gender':msg['User']['Sex'],'Status':True,'ToUser':''}
              People.append(msg['FromUserName'])
              if len(People)==1:
                  print('人不够')
              itchat.send_msg('正在等待匹配.您也可以随时输入[离开]退出等待。',toUserName=msg['FromUserName'])
-             pair(msg)
-def pair(msg):
+           #  pair()
+def pair():
     global Room_Profile
     global Room_id
     global People
     global PeopleAll
-        print(Room_Profile)
-        print(Room_id)
-        print(PeopleAll)
-        print(People)
+   # print(PeopleAll)
+   # print(People)
+  #  print(0)
+    while(len(People)<=1):
+        time.sleep(10)
+     #   print(0.1)
+    while(len(People)>=2):
+        x=range(len(People))
+     #   print(1)
+        x.pop(0)
+        R=random.choice(x)
+        #People[0]+People[R]
+        if PeopleAll[People[0]]['Status']==True and PeopleAll[People[R]]['Status']==True:
+            print('匹配成功')
+            PeopleAll[People[0]]['Status']==False
+            PeopleAll[People[R]]['Status']==False
+            PeopleAll[People[0]]['ToUser']==People[R]
+            PeopleAll[People[R]]['ToUser']==People[0]
+            #进入游戏
+            if PeopleAll[People[R]]['Gender']==1:
+                Gender='男生'
+            elif PeopleAll[People[R]]['Gender']==2:
+                Gender='女生'
+            else:
+                Gender='性别未知'
+            itchat.send_msg('匹配成功,对方信息:'+Gender,toUserName=People[0])
+            if PeopleAll[People[0]]['Gender']==1:
+                Gender='男生'
+            elif PeopleAll[People[0]]['Gender']==2:
+                Gender='女生'
+            else:
+                Gender='性别未知'
+            itchat.send_msg('匹配成功,对方信息:'+Gender,toUserName=People[R])
+            GamePeople.append(People[0])
+            GamePeople.append(People[R])
+        if PeopleAll[People[0]]['Status']!=True:
+            People.pop(0)
+        if PeopleAll[People[R]]['Status']!=True:
+            People.pop(R)
+    print(PeopleAll)
+    print(People)
+    pair()
 itchat.run()
 '''
 随机分配有可能会重复，存在问题。
